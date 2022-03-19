dc75827b8fa0e7cdae81982e2420f954942016eb
FangFangHawk <94452477+FangFangHawk@users.noreply.github.com>
2022-03-19 17:55:16 +0800

    Add files via upload

---
+++ demo.c
@@ -0,0 +1,25 @@
+
+int main()
+{
+int a  = 132.1321;
+a += 00;
+a += 00101;
+a = 0x10a1;
+a = 10.0e+10;
+a = 10e-123;
+a = 10e+123;
+a = 123456;
+long b == 0x8;
+double c >>= 0b101;
+int d = a + b;
+char q = 'a';
+string asd_aasd_1123 = "asdads";
+scanf("%d " , &a);
+printf("%d" , a); //This is a test note
+/*This is a test note
+asdad //nothing thing
+asdsaddsa
+*/
+
+return 0; 
+}
